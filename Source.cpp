a0349af51869de3148c5ca93bf89ba98ac2a1fe4
Dominic-Hoe <domhoe2004@gmail.com>
2021-02-10 00:46:10 +0000

    More comments.

--- Source.cpp
+++ Source.cpp
@@ -17,8 +17,15 @@ int main() {
 	*ptr = 10;
 	// As mentioned before, ptr is just a reference to var. Pointers store memory addresses, which can be read from and written to.
 	// The "*ptr" shown above is known as "dereferencing" the pointer, meaning that what's *actually* being overwritten is the
-	// original value var. When logging var, the output will be 10
+	// original value var. When logging var, the output will be 10.
+
+	// In essence, what's happening here is calling *ptr says "Hey, get me this variable from this memory address" and then that
+	// memory address can be written to/read from.
 	LOG(var);
+	LOG(*ptr);
+	// This yields 1, which, in boolean sense, means yes, because 0 = false and 1 = true. "var" is the same as "ptr" because
+	// "ptr" is just a reference to var!
+	LOG((var == *ptr));
 	std::cin.get();
 	return 0;
 }
